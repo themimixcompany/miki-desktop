871ee7b7b42fc5c478c61a9578afc085e612490f
Rommel MARTINEZ <rom@mimix.io>
2021-03-15 23:08:18 +0800

    main.js: move postgres to appdata

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-const VERSION = "2.2.0";
+const VERSION = "2.3.0";
 
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
@@ -8,7 +8,7 @@ const portscanner = require('portscanner');
 const sleep = require('system-sleep');
 
 const HOST = process.env.HOST || '127.0.0.1';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const MIKIROOT = path.resolve(__dirname, 'miki');
 
 var PG_PATH;
@@ -29,38 +29,41 @@ function debug (error, stdout, stderr) {
 }
 
 function initDatabase () {
-  console.log("** Initializing database...");
-  spawnSync(`${PG_PATH}/bin/initdb`, ['-U', PG_USER, '-A', 'trust', '-D', `${PG_PATH}/data`]);
+  console.log("** initDatabase start...");
+  spawnSync(`${PG_PATH}/bin/initdb`,
+            ['-U', PG_USER, '-A', 'trust', '-D', `${PG_PATH}/data`]);
+  console.log("** initDatabase done.");
 }
 
 function startDatabase () {
-  console.log("** Starting database server...");
+  console.log("** startDatabase start...");
   spawn(`${PG_PATH}/bin/pg_ctl`, ['start', '-D', `${PG_PATH}/data`]);
-  sleep(5*1000);
+  console.log("** startDatabase done.");
 }
 
 function createDatabase () {
-  console.log("** Creating main database...");
-  spawn(`${PG_PATH}/bin/createdb`, ['-h', PG_HOST, '-p', PG_PORT, '-U', PG_USER, PG_DATABASE]);
+  console.log("** createDatabase start...");
+  spawnSync(`${PG_PATH}/bin/createdb`, ['-h', PG_HOST, '-p', PG_PORT, '-U', PG_USER, PG_DATABASE]);
+  console.log("** createDatabase done.");
 }
 
 function execSQL(statement) {
-  spawn(`${PG_PATH}/bin/psql`,
-        ['-h', PG_HOST, '-p', PG_PORT, '-U', PG_USER, '-d', PG_DATABASE, '-c', statement]);
+  spawnSync(`${PG_PATH}/bin/psql`,
+            ['-h', PG_HOST, '-p', PG_PORT, '-U', PG_USER, '-d', PG_DATABASE, '-c', statement]);
 }
 
 function setupDatabase () {
-  console.log("** Setting up the main database...");
+  console.log("** setupDatabase start...");
   execSQL(`GRANT ALL PRIVILEGES ON DATABASE ${PG_DATABASE} TO ${PG_USER};`);
-  // execSQL(`UPDATE users SET email = 'support@mimix.io' WHERE id = '1';`);
-  // execSQL(`UPDATE users SET password = '0123456789' WHERE id = '1';`);
-  // execSQL(`UPDATE users SET "mustChangePwd" = 't' WHERE id = '1';`);
+  console.log("** setupDatabase done.");
 }
 
 function startPostgresWindows () {
   console.log("** Starting Postgres for Windows...");
 
-  PG_PATH = path.resolve(__dirname, 'pgsql/windows');
+  const appdata = process.env.APPDATA;
+
+  PG_PATH = path.resolve(`${appdata}/Mimix/pgsql`);
 
   if(fs.existsSync(`${PG_PATH}/data`)) {
     startDatabase();
@@ -106,21 +109,28 @@ function createWindow () {
 }
 
 function runApp () {
-  let stat;
+  let postgresStat;
+  let mikiStat;
 
   startPostgres();
   startMiki();
 
   outer:
   while (true) {
-    portscanner.checkPortStatus(PORT, HOST, (error, status) => {
-      stat = status;
+    portscanner.checkPortStatus(PG_PORT, PG_HOST, (error, status) => {
+      postgresStat = status;
     });
 
-    if (stat == 'open') {
+    portscanner.checkPortStatus(PORT, HOST, (error, status) => {
+      mikiStat = status;
+    });
+
+    if (postgresStat == 'open' && mikiStat == 'open') {
       sleep(5*1000);
       break outer;
     } else {
+      console.log(`postgresStat: ${postgresStat}`);
+      console.log(`mikiStat: ${mikiStat}`);
       sleep(1*1000);
     }
   }
